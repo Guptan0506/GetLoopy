871e979273d675c878ddf397dde2a5f909d27256
Guptan0506 <navyag0509@gmail.com>
2024-10-14 00:43:58 -0400

    commit

--- src/Task8.java
+++ src/Task8.java
@@ -3,14 +3,23 @@ import java.util.Random;
 public class Task8 {
     public static void main(String[] args){
         Random rand = new Random();
-        int d = rand.nextInt(1, 6);
-        int e = rand.nextInt(1, 6);
-        int f = rand.nextInt(1, 6);
+        int d = 0;
+        int e = 1;
+        int f = 2;
         int counter = 0;
-        System.out.println("Roll Die 1, Die 2 and Die 3");
-        for (d == e ==f){
-            counter++;
-            System.out.printf(counter + "   "+ d + "  "+ e + "  "+ f + "  "+ (d+e+f));
+        int sum;
+        boolean triple = false;
+        System.out.println("Roll   Die1   Die2  Die3  Sum");
+        System.out.println("-----------------------------");
+        for (;!triple; counter++){
+            d = rand.nextInt(6)+1;
+            e = rand.nextInt(6)+1;
+            f = rand.nextInt(6)+1;
+            sum = d + e +f;
+            System.out.println(counter + "       "+ d + "      "+ e + "     "+ f + "     "+ sum);
+            if (d == e && e==f){
+                triple = true;
+            }
         }
     }
 }
