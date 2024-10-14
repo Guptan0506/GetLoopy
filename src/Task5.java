18f2c4a301da45cfbd7cce952f76681402661551
Guptan0506 <navyag0509@gmail.com>
2024-10-14 00:10:26 -0400

    commit

---
+++ src/Task5.java
@@ -0,0 +1,10 @@
+public class Task5 {
+    public static void main(String[] args){
+        for (int m = 0; m < 6; m ++ ) {
+            for (int n = 0; n <= 1; n ++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
